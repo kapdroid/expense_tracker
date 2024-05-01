a85f58498c4b4abf7ecaa33f64579f609ffc2ba2
kapdroid <kapilsharma130519@gmail.com>
2024-05-01 16:05:01 +0530

    topic: add title and appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:expense_tracker/widgets/expense.dart';
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(Expenses());
+void main() {
+  runApp(
+    MaterialApp(theme: ThemeData(useMaterial3: true),
+      home: Expenses()),
+  );
 }
--- lib/widgets/expense.dart
+++ lib/widgets/expense.dart
@@ -30,14 +30,21 @@ class _ExpenseState extends State<Expenses> {
   ];
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-          body: Column(
-        children: [
-          const Text("Expense Tracker"),
-          ExpenseList(_registeredExpenses)
-        ],
-      )),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Expense Tracker"),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.add),
+            )
+          ],
+        ),
+        body: Column(
+          children: [
+            const Text("Expense Tracker"),
+            ExpenseList(_registeredExpenses)
+          ],
+        ));
   }
 }
