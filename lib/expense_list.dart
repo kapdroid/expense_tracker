18bef912ac9cb7e3b4b1e0cd41eec9d495629d2d
kapdroid <kapilsharma130519@gmail.com>
2024-05-01 13:11:46 +0530

    topic: expense using listView

---
+++ lib/expense_list.dart
@@ -0,0 +1,15 @@
+import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class ExpenseList extends StatelessWidget {
+  const ExpenseList(this._expenseList, {super.key});
+
+  final List<Expense> _expenseList;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: _expenseList.length,
+        itemBuilder: (ctx, index) => Text(_expenseList[index].name));
+  }
+}
