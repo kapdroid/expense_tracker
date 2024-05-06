95e32327fab6878558ed324d8aa5ca589d5983dd
kapdroid <kapilsharma130519@gmail.com>
2024-05-06 16:41:29 +0530

    topic: LayoutBuilder

--- lib/widgets/add_expense.dart
+++ lib/widgets/add_expense.dart
@@ -49,80 +49,134 @@ class _AddExpenseState extends State<AddExpense> {
   @override
   Widget build(context) {
     final keyboardSpace = MediaQuery.of(context).viewInsets.bottom;
-    return SizedBox(
-      height: double.infinity,
-      child: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.fromLTRB(4, 4, 4, keyboardSpace + 4),
-          child: Column(
-            children: [
-              TextField(
-                controller: _textEditingController,
-                decoration: const InputDecoration(
-                  label: Text("Title"),
-                ),
-                maxLength: 50,
-                keyboardType: TextInputType.multiline,
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                        controller: _amoutEditingController,
-                        decoration: const InputDecoration(
-                            label: Text("Amount "), prefixText: "\$ "),
-                        maxLength: 6,
-                        keyboardType: TextInputType.number),
-                  ),
-                  const SizedBox(
-                    width: 10,
-                  ),
-                  Expanded(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Text(_selectedDate == null
-                            ? "No Date Selected"
-                            : formatter.format(_selectedDate!)),
-                        IconButton(
-                            onPressed: _selectExpenseDate,
-                            icon: const Icon(Icons.calendar_month))
-                      ],
+    return LayoutBuilder(builder: ((context, constraints) {
+      final width = constraints.maxWidth;
+      return SizedBox(
+        height: double.infinity,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(4, 4, 4, keyboardSpace + 4),
+            child: Column(
+              children: [
+                if (width > 600)
+                  Row(
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          controller: _textEditingController,
+                          decoration: const InputDecoration(
+                            label: Text("Title"),
+                          ),
+                          maxLength: 50,
+                          keyboardType: TextInputType.multiline,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Expanded(
+                        child: TextField(
+                            controller: _amoutEditingController,
+                            decoration: const InputDecoration(
+                                label: Text("Amount "), prefixText: "\$ "),
+                            maxLength: 6,
+                            keyboardType: TextInputType.number),
+                      ),
+                    ],
+                  )
+                else
+                  TextField(
+                    controller: _textEditingController,
+                    decoration: const InputDecoration(
+                      label: Text("Title"),
                     ),
+                    maxLength: 50,
+                    keyboardType: TextInputType.multiline,
                   ),
-                  // IconButton(
-                  //     onPressed: () {}, icon: const Icon(Icons.calendar_month))
-                ],
-              ),
-              Row(
-                children: [
-                  DropdownButton(
-                      value: _selectedCategory,
-                      items: Category.values
-                          .map((item) => DropdownMenuItem(
-                                value: item,
-                                child: Text(item.name.toUpperCase()),
-                              ))
-                          .toList(),
-                      onChanged: (value) {
-                        _selectCategory(value as Category);
-                      }),
-                  TextButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      child: const Text("Cancel")),
-                  ElevatedButton(
-                      onPressed: validateInputs,
-                      child: const Text("Add Expense"))
-                ],
-              )
-            ],
+                Row(
+                  children: [
+                    if (width > 600)
+                      DropdownButton(
+                          value: _selectedCategory,
+                          items: Category.values
+                              .map((item) => DropdownMenuItem(
+                                    value: item,
+                                    child: Text(item.name.toUpperCase()),
+                                  ))
+                              .toList(),
+                          onChanged: (value) {
+                            _selectCategory(value as Category);
+                          })
+                    else
+                      Expanded(
+                        child: TextField(
+                            controller: _amoutEditingController,
+                            decoration: const InputDecoration(
+                                label: Text("Amount "), prefixText: "\$ "),
+                            maxLength: 6,
+                            keyboardType: TextInputType.number),
+                      ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Expanded(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(_selectedDate == null
+                              ? "No Date Selected"
+                              : formatter.format(_selectedDate!)),
+                          IconButton(
+                              onPressed: _selectExpenseDate,
+                              icon: const Icon(Icons.calendar_month))
+                        ],
+                      ),
+                    ),
+                    // IconButton(
+                    //     onPressed: () {}, icon: const Icon(Icons.calendar_month))
+                    if (width > 600)
+                      TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Text("Cancel")),
+                    if (width > 600)
+                      ElevatedButton(
+                          onPressed: validateInputs,
+                          child: const Text("Add Expense"))
+                  ],
+                ),
+                if (width <= 600)
+                  Row(
+                    children: [
+                      DropdownButton(
+                          value: _selectedCategory,
+                          items: Category.values
+                              .map((item) => DropdownMenuItem(
+                                    value: item,
+                                    child: Text(item.name.toUpperCase()),
+                                  ))
+                              .toList(),
+                          onChanged: (value) {
+                            _selectCategory(value as Category);
+                          }),
+                      TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Text("Cancel")),
+                      ElevatedButton(
+                          onPressed: validateInputs,
+                          child: const Text("Add Expense"))
+                    ],
+                  )
+              ],
+            ),
           ),
         ),
-      ),
-    );
+      );
+    }));
   }
 
   void validateInputs() {
